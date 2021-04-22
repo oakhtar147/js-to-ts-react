bf301bc4121d560d0e0bef36f85094f54040f837
oakhtar147 <osamaakhtar15@gmail.com>
2021-04-23 01:17:38 +0500

    iterate

--- src/scratch.ts
+++ src/scratch.ts
@@ -1,89 +1,89 @@
-interface Person {
-  name: {
-    first: string;
-    middle?: string;
-    last: string;
-  };
-  address: {
-    house: number;
-    apartment?: {
-      number: number;
-      suite: Boolean;
-    };
-    street: string;
-  };
-}
+// interface Person {
+//   name: {
+//     first: string;
+//     middle?: string;
+//     last: string;
+//   };
+//   address: {
+//     house: number;
+//     apartment?: {
+//       number: number;
+//       suite: Boolean;
+//     };
+//     street: string;
+//   };
+// }
 
-const Osama: Person = undefined as any;
+// const Osama: Person = undefined as any;
 
-// optional chaining
-console.log(Osama.address.apartment?.suite);
+// // optional chaining
+// console.log(Osama.address.apartment?.suite);
 
-class Foo {
-  // hard private - cannot be accessed through breakpoints
-  #name: string;
-  constructor(rawName?: string) {
-    // nullish coalescing
-    this.#name = rawName ?? '(no name)';
-  }
-  log() {
-    console.log(this.#name);
-  }
-}
+// class Foo {
+//   // hard private - cannot be accessed through breakpoints
+//   #name: string;
+//   constructor(rawName?: string) {
+//     // nullish coalescing
+//     this.#name = rawName ?? '(no name)';
+//   }
+//   log() {
+//     console.log(this.#name);
+//   }
+// }
 
-type SomeTupleType<T extends any[]> = [boolean, ...T, boolean];
+// type SomeTupleType<T extends any[]> = [boolean, ...T, boolean];
 
-// labeled tuples
+// // labeled tuples
 
-type Gym = [
-  tired: boolean,
-  targetMuscle: string,
-  hours: number,
-  diet: string,
-];
+// type Gym = [
+//   tired: boolean,
+//   targetMuscle: string,
+//   hours: number,
+//   diet: string,
+// ];
 
-const Workout: Gym = [true, 'legs', 1.25, 'eggs'];
+// const Workout: Gym = [true, 'legs', 1.25, 'eggs'];
 
-function processWorkout(...gym: Gym) {}
-processWorkout(...Workout); // poor tooltip labelling of args -> use l  abelled tuples
+// function processWorkout(...gym: Gym) {}
+// processWorkout(...Workout); // poor tooltip labelling of args -> use l  abelled tuples
 
-// Recursive types
+// // Recursive types
 
-type JSONValue =
-  | string
-  | number
-  | boolean
-  | null
-  | JSONValue[]
-  | {
-      [k: string]: JSONValue;
-    };
+// type JSONValue =
+//   | string
+//   | number
+//   | boolean
+//   | null
+//   | JSONValue[]
+//   | {
+//       [k: string]: JSONValue;
+//     };
 
-const val: JSONValue = {
-  name: 'osama',
-  address: {
-    street: 7,
-  },
-};
+// const val: JSONValue = {
+//   name: 'osama',
+//   address: {
+//     street: 7,
+//   },
+// };
 
-function somethingRisky() {}
-function isError(err: any): err is Error {
-  return err instanceof Error;
-}
+// function somethingRisky() {}
+// function isError(err: any): err is Error {
+//   return err instanceof Error;
+// }
 
-try {
-  somethingRisky();
-} catch (err) {
-  console.log(err.stack); // this property may or may not exist
-}
+// try {
+//   somethingRisky();
+// } catch (err) {
+//   console.log(err.stack); // this property may or may not exist
+// }
 
-// better -> use unknown as a type guard
-try {
-  somethingRisky();
-} catch (err: unknown) {
-  if (isError(err)) {
-    console.log(err.stack);
-  } else {
-    console.log(err);
-  }
-}
+// // better -> use unknown as a type guard
+// try {
+//   somethingRisky();
+// } catch (err: unknown) {
+//   if (isError(err)) {
+//     console.log(err.stack);
+//   } else {
+//     console.log(err);
+//   }
+// }
